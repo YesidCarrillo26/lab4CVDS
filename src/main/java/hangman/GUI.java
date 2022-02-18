f7273fb7d3c8e4c3a8f04a5830d7612909267834
YesidCarrillo26 <yesid.carrillo@mail.escuelaing.edu.co>
2022-02-17 20:13:53 -0500

    QUE VIVA CLASH ROYALEEEEEE

--- src/main/java/hangman/GUI.java
+++ src/main/java/hangman/GUI.java
@@ -34,6 +34,9 @@ public class GUI {
     private HangmanDictionary dictionary;
     private HangmanPanel hangmanPanel;
 
+    private GameScore gameScore;
+    private OriginalScore originalScore;
+
     private MainFrameController mainFrameController;
 
     private SplashController splashController;
@@ -48,14 +51,17 @@ public class GUI {
         this.language = factoryMethod.createLanguage();
         this.dictionary = factoryMethod.createDictionary();
         this.hangmanPanel = factoryMethod.createHangmanPanel();
+        //this.originalScore = factoryMethod.createOriginalScore();
+        this.gameScore = factoryMethod.createGameScore();
     }
 
     @Inject
     // Use Guice constructor
-    public GUI(Language language, HangmanDictionary dictionary, HangmanPanel hangmanPanel){
+    public GUI(Language language, HangmanDictionary dictionary, HangmanPanel hangmanPanel, GameScore gameScore){
         this.language = language;
         this.dictionary= dictionary;
         this.hangmanPanel = hangmanPanel;
+        this.gameScore = gameScore;
     }
 
     //method: setup
